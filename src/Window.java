e41e089d8a575bc8f65fae535678c303ebfd06a1
xym4er <xym4er@gmail.com>
2015-09-24 11:18:10 +0300

    123

--- src/Window.java
+++ src/Window.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.Scanner;
 
 /**
@@ -6,13 +7,29 @@ import java.util.Scanner;
  */
 public class Window extends JFrame {
     public static JTextArea jTextArea;
+    public static JLabel jLabel;
 
     public Window() {
         setSize(400, 400);
         setTitle("Sea Battle");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+        setDefaultLookAndFeelDecorated(true);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+
+
         jTextArea = new JTextArea();
-        add(jTextArea);
+        add(jTextArea, BorderLayout.PAGE_START);
+        jTextArea.setVisible(true);
+        jLabel = new JLabel();
+        add(jLabel, BorderLayout.CENTER);
+        jLabel.setText("Тестовый текст!!!!!!");
+        jLabel.setBounds(100, 50, 100, 50);
+        jTextArea.setEditable(false);
+        jTextArea.setLocation(0,0);
+
+
+
     }
 }
